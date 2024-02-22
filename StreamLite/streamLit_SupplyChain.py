a1fcc86392bd8303b68006b81c28e70317b84d39
jo77Lu <129406382+jo77Lu@users.noreply.github.com>
2024-02-23 00:18:18 +0100

    Update streamLit_SupplyChain.py: modification de la section modelisation 2 et ajout de fonctions annexes afferentes

--- StreamLite/streamLit_SupplyChain.py
+++ StreamLite/streamLit_SupplyChain.py
@@ -1,16 +1,127 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 21 10:07:36 2022
+Created on Thu feb 21 10:07:36 2024
 
-@author: yohancohen
+@author: joanAsselot & MickaelGalliez
 """
 
 import streamlit as st
 import pandas as pd
 import numpy as np
+import re
+import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from gensim.models import FastText
 
+
+# Téléchargez la liste de stop words en français
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Importez la liste de stop words en français
+from nltk.corpus import stopwords
+
+@st.cache_resource
+def get_df():
+    """
+    charge le dataframe et applique les dernieres etapes de nettoyage.
+    doit etre applique 1 fois au debut de la fonction principale pour charge le dataframe dans le cache.
+
+    :return: dataframe
+    """
+
+    df = pd.read_csv("df_featEngin.csv")
+
+    return df
+@st.cache_resource
+def get_dfFastText():
+    """
+    Retourne un dataframe avec les colonnes commentaires et titres vectorises.
+    Cette fonction est utile pour les sections modelisation II et III.
+    On appelle la fonction en debut de fonction principale pour charger les donnees dans le cache
+    :return:
+    """
+
+    df = get_df()
+
+    df['embedding_Commentaire'] = df['Commentaire'].apply(get_text_embedding)
+    df['embedding_Titres'] = df['Titres'].apply(get_text_embedding)
+
+    df_expanded = df_test['embedding_Commentaire'].apply(lambda x: pd.Series(x, dtype="float64")).add_prefix(
+        'vectComment_')
+    df_expanded2 = df_test['embedding_Titres'].apply(lambda x: pd.Series(x, dtype="float64")).add_prefix('vectTitre_')
+
+    df = pd.concat([df_expanded,df_expanded2,df[['Nombre_avis_publie','longCommentaire','longTitres','nb_Mots_Commentaire',
+        'nb_Mots_Titres', 'nb_majuscules_Commentaire',
+           'nb_chiffres_Commentaire', 'nb_ponctuation_Commentaire',
+           'nb_special_Commentaire', 'nb_majuscules_Titre', 'nb_chiffres_Titre',
+           'nb_ponctuation_Titre', 'nb_special_Titre', 'Sentiment']]],axis=1)
+
+
+@st.cache_resource
+def get_fastTextModel():
+    """
+    retourne le model fast text. Ce model est long a construire aussi nous utiliserons le decorateur cache ressource
+    cette fonction doit etre appele une premiere fois au debut de la fonction principale afin de charge le model dans le cache
+    elle pourra etre appele a nouveau par la suite dans les differentes sections sans temps de chargement.
+    :return: model fast text
+    """
+    # Replace 'path/to/pretrained/embeddings' with the actual path to your downloaded embeddings file
+    return FastText.load_fasttext_format("FastText/cc.fr.300.bin.gz")
+
+def get_splited_df(data,target):
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2,
+                                                        random_state=42)
+    return X_train, X_test, y_train, y_test
+def preprocess_text(text):
+    """
+    Applique un nettoyage puis tokenize le texte. Etape necessaire avant la vectorization
+    :param text: String
+    :return: [string] list of string
+    """
+    stop_words = set(stopwords.words('french'))
+
+    tokens = nltk.tokenize.word_tokenize(text)
+    tokens = [word.lower() for word in tokens if word.isalpha() and word.lower() not in stop_words]
+    return tokens
+
+def get_text_embedding(text):
+    """
+    Vectorize le text a partir du model preentrainne Fast-text
+    :param text: [String] List of String
+    :return: [float] Vector list of float
+    """
+    model = get_fastTextModel()
+
+    preprocessed_text = preprocess_text(text)
+    word_embeddings = [model.wv[word] for word in preprocessed_text if word in model.wv.key_to_index]
+
+    if word_embeddings:
+        text_embedding = np.mean(word_embeddings, axis=0)  # Use mean for aggregation
+    else:
+        text_embedding = np.zeros(model.vector_size)  # Use zero vector if no embeddings found
+
+    return text_embedding
+
+
+
+#############################################################################################################
+#                           Fonctions de Sections
+#############################################################################################################
 def demo_intro():
+    """
+    Definit la section Introduction du rapport
+    :return: None
+    """
+
     st.write("### Introduction")
     st.image("./wordCloud.png")
 
@@ -20,7 +131,7 @@ def demo_intro():
              "Positif ou Negatif.\n")
 
 def demo_explor():
-    df = pd.read_csv("df_featEngin.csv")
+    df = get_df()
 
     st.write("### Exploration")
     st.write("#### WebScrapping")
@@ -40,6 +151,34 @@ def demo_model1():
 
 def demo_model2():
     st.write("### Modelisation II")
+    tab1, tab2 = st.tabs(["Random Forest", "XGBoost"])
+    with tab1:
+        col01, col02, col03 = st.columns(3)
+        with col01:
+            st.write("Hyperparametres:")
+        with col03:
+            isBest = st.checkbox('Selection automatique')
+
+        col1,col2,col3,col4  = st.columns(4)
+        if isBest:
+            with col1:
+                n = st.selectbox('n_estimator',[100], disabled=True)
+            with col2:
+                split = st.selectbox('min_samples_leaf',[2], disabled=True)
+            with col3:
+                leaf = st.selectbox('min_samples_leaf',[5], disabled=True)
+            with col4:
+                feat = st.selectbox('max_features',["auto"], disabled=True)
+        else:
+            with col1:
+                n = st.selectbox('n_estimator', [100, 200, 500])
+            with col2:
+                split = st.selectbox('min_samples_leaf', [2, 5, 10])
+            with col3:
+                leaf = st.selectbox('min_samples_leaf', [1, 5, 10])
+            with col4:
+                feat = st.selectbox('max_features', ["auto", "sqrt", "log2"])
+
 
 def demo_interact():
     st.write("### Interactivite")
@@ -54,7 +193,16 @@ def demo_interact():
     st.write(input)
 
 
+
+#############################################################################################################
+#                           Fonction Principale
+#############################################################################################################
 def demo_supplyChain():
+
+    #Chargement donnees dans le cache:
+    get_df()
+    #get_fastTextModel() # Cette etape necessitte 5 a 10 min de chargement.
+    #get_dfFastText()    # L'etape precedente est necessaire pour effectue celle ci
 
     #Side bar - Sommaire:
 
